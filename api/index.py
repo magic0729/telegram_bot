069eb616ab5d3aa442d257ffe99b96f690d6517a
magic0729 <kok620-4@outlook.com>
2025-11-06 04:05:01 +0900

    fix: delete no needed file

--- api/index.py
+++
@@ -1,9 +0,0 @@
-"""
-Vercel serverless function handler for Flask app
-This is the entry point for Vercel serverless functions
-"""
-from app_ui import app
-
-# Vercel expects the Flask app to be accessible
-# The app will be automatically detected by @vercel/python
-
